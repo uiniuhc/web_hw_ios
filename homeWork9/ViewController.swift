1c04745628d55b29318466cdb4ae0ef34f2bf916
hanqiu <chuiniu1st@gmail.com>
2016-11-27 23:09:51 -0800

    Initial Commit

---
+++ homeWork9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homeWork9
+//
+//  Created by Zhang.Hanqiu on 11/27/16.
+//  Copyright © 2016 hanqiu_have_fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
